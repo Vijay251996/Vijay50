74cc1f8c6e088d64f5c1b1b580952f8f24446665
vijay251996 <vijayrathore23hld@gmail.com>
2021-09-25 13:37:09 +0530

    parameter added

--- src/test/java/mavenforJenkins/Login1.java
+++ src/test/java/mavenforJenkins/Login1.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,16 +15,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Login1 {
 	
 	static WebDriver driver;
+	@Parameters("Browser")
 	@Test
-	public static void launch() {
+	public static void launch(String Browsername) {
+		
+		System.out.println("The browser name is :"+ Browsername);
+		if(Browsername.contains("chrome"))
+		{
 		WebDriverManager.chromedriver().setup();
-
-		driver = new ChromeDriver();
+        driver = new ChromeDriver();
+		}
+		else if(Browsername.contains("mozilla"))
+		{
+		WebDriverManager.firefoxdriver().setup();
+         driver = new FirefoxDriver();
+		}
+		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	
 	}
    @Test
-	public static void login1() {
+	public static void logi1() {
 		driver.get("http://www.demo.guru99.com/V4/");
 		driver.findElement(By.cssSelector("input[name='uid']")).sendKeys("mngr353182");
 		driver.findElement(By.cssSelector("input[name='password']")).sendKeys("urAbYpU");
